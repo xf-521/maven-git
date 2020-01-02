1eca3d5718b1fe1fe4aab2f96cebc8cd175dc2a2
xf-521 <773775578@qq.com>
2020-01-02 11:56:39 +0800

    初始化

---
+++ src/main/java/day05/Test.java
@@ -0,0 +1,15 @@
+package day05;
+
+/**
+ * @author xiaofeng
+ *
+ * @date 2020年1月2日
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("ss");
+		
+	}
+}
